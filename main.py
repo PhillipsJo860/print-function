2deab9ff4d1b762bfac29935e867f47523c03d4c
SoggyCereal <PhillipsJo860@northwested.org>
2024-09-09 10:26:57 -0400

    Create main.py
    Added content

---
+++ main.py
@@ -0,0 +1,22 @@
+# Joshua Phillips
+# 9/9/24
+# Printing in python
+
+# Exercise 2 - 3  (Personal Message)
+person1 = 'Lucas'
+
+print (f'Hello {person1}, are you ready?')
+
+
+#  Exercise 2 - 5 (Famous Quote)
+quote = 'If you want something done right, do it yourself.'
+famous_person = 'Charles-Guillaume Étienne'
+
+print (f'{famous_person} made the famous quote: {quote} I take this quote to heart when it comes to most things.')
+
+# Exercise 2 - 6 (Famous Quote 2)
+
+message = 'If you want something done right, do it yourself.'
+famous_person = 'Charles-Guillaume Étienne'
+
+print (f'{famous_person} made the famous quote: {message} I take this quote to heart when it comes to most things.')
